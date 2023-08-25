c9902e61b04a1d86423b58354d781e5173af3be0
agatahani <103161198+agatahani@users.noreply.github.com>
2023-08-25 07:42:09 +0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "Basic Sphinx Example Project"
-copyright = "2022, Read the Docs core team"
-author = "Read the Docs core team"
+project = "Craigslist Documentation Project"
+copyright = "2023, Craigslist Documentation team"
+author = "Craigslist Documentation team"
 
 
 # -- General configuration ---------------------------------------------------
